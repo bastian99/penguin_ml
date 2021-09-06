66029285ccddb83eed318a3cff97eaa4acff4a27
bastian99 <bastianser924@gmail.com>
2021-09-06 11:49:32 -0500

    passw

--- penguins_streamlit.py
+++ penguins_streamlit.py
@@ -3,11 +3,22 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 import pandas as pd
 import pickle
+from sklearn.metrics import accuracy_score
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
 
-st.title('Penguin Classifier: A Machine Learning App')
+
+st.title('Penguin Classifier')
 st.write("This app uses 6 inputs to predict the species of penguin using "
          "a model built on the Palmer's Penguin's dataset. Use the form below"
-         " to get started!")
+         " to get started!")    
+
+password_guess = st.text_input('What is the Password?')
+if password_guess != st.secrets["password"]:
+  st.stop()
+
+penguin_file = st.file_uploader('Upload your own penguin data')
+
 penguin_df = pd.read_csv('penguins.csv')
 rf_pickle = open('random_forest_penguin.pickle', 'rb')
 map_pickle = open('output_penguin.pickle', 'rb')
